eeec4663334473e517b4d9322eeafeaf24602008
Jan <github@geheimwerk.de>
2011-12-02 16:06:16 +0100

    Adding support for even more invisible characters.

--- JXInvisiCharLayoutManager.m
+++ JXInvisiCharLayoutManager.m
@@ -15,13 +15,19 @@ typedef struct _JXUnicharMappingStruct {
 
 JXUnicharMappingStruct JXInvisiCharToCharMap[] = {
 	//	invisible	replacement
-	{	 ' ',		0x002E}, // ordinary space	TO	ordinary full stop; alternatives: one dot leader (0x2024), small full stop (0xFE52)
-	{	0x0A,		0x2025}, // no-break space	TO	two dot leader
-	{	'\t',		0x21E5}, // tab				TO	rightwards arrow to bar; alternatives: rightwards arrow (0x2192), rightwards dashed arrow (0x21E2)
-	{	'\n',		0x00B6}, // new line		TO	pilcrow sign
-	{	'\r',		0x204B}, // carriage return	TO	reversed pilcrow sign
-	{	0x0C,		0x21DF}, // page break		TO	downwards arrow with double stroke; alternatives: next page (0x2398)
+	{		 ' ',		0x002E}, // ordinary space				TO	ordinary full stop; alternatives: one dot leader (0x2024), small full stop (0xFE52)
+	{	  0x00A0,		0x237D}, // no-break space				TO	shouldered open box
 	
+	{		'\t',		0x21E5}, // tab							TO	rightwards arrow to bar; alternatives: rightwards arrow (0x2192), rightwards dashed arrow (0x21E2)
+	
+	{		'\n',		0x00B6}, // new line					TO	pilcrow sign
+	{		'\r',		0x204B}, // carriage return				TO	reversed pilcrow sign
+	{	  0x2028,		0x2761}, // unicode line separator		TO	curved stem paragraph sign ornament
+	{	  0x2029,		0x21AB}, // unicode paragraph separator	TO	leftwards arrow with loop
+	
+	{	  0x000C,		0x21DF}, // page break					TO	downwards arrow with double stroke; alternatives: next page (0x2398)
+	
+	{	  0xFFFF,		0xFFFD}, // invalid control				TO	replacement character
 };
 
 
