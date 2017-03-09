17a3e8518d7300c32ee230921e9d0d809381bacc
Jan <github@geheimwerk.de>
2017-03-09 14:32:55 +0100

    Updating comment.

--- JXNoBreaksTypesetter.h
+++ JXNoBreaksTypesetter.h
@@ -11,7 +11,7 @@
  This Typesetter prevents lines from breaking on paragraph and line break marks (i.e. '\n', etc.).
  It’s a bit crude in the way it does this. For example it currently just reserves *some* space 
  that the paragraph marks introduced by JXInvisiCharLayoutManager will fit in. Furthermore 
- it doesn’t handle editing very well. There are visual issues when editing results in 
+ it doesn’t handle editing very well. There may be/used to be visual issues when editing results in 
  added or removed breaks.
  The *raison d’être* for JXNoBreaksTypesetter is displaying as much text 
  (with control characters made visible) as can reasonably fit into a single line.
